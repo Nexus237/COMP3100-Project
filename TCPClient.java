5161c71e140e18f1c2b76b1fb09fec7a4e8cc55e
Nexus237 <rohangeorge201@gmail.com>
2022-03-17 01:51:16 +1100

    Basic implementation of authentication & code refactoring (TCPClient)

--- TCPClient.java
+++ TCPClient.java
@@ -4,37 +4,40 @@ import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 public class TCPClient{
-        public static void main(String args[]){
-		while  (true) {
-			try{
-				Socket s = new Socket("127.0.0.1",50000);
-				DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-				BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));
+  public static void main(String args[]){
+    while  (true) {
+      try{
+        Socket s = new Socket("127.0.0.1",50000);
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-                                dout.write(("HELO\n").getBytes());
-                                System.out.println("SENT: HELO");
+        dout.write(("HELO\n").getBytes());
+        System.out.println("SENT: HELO");
 
-                                String str = (String)din.readLine();
-                                System.out.println("RCVD: "+str);
+        String str = (String)din.readLine();
+        System.out.println("RCVD: "+str);
 
-                                dout.write(("REDY\n").getBytes());
-                                System.out.println("SENT: REDY");
+        String username = System.getProperty("user.name");
+        dout.write(("AUTH " + username + "\n").getBytes());
 
-                                String str = (String)din.readLine();
-                                System.out.println("RCVD: "+str);
+        dout.write(("REDY\n").getBytes());
+        System.out.println("SENT: REDY");
 
-                                dout.write(("QUIT\n").getBytes());
-                                System.out.println("SENT: QUIT");
+        str = (String)din.readLine();
+        System.out.println("RCVD: "+str);
 
-                                str = (String)din.readLine();
-                                System.out.println("RCVD: "+str);
+        dout.write(("QUIT\n").getBytes());
+        System.out.println("SENT: QUIT");
 
-				dout.close();
-				s.close();
-			}
-			catch(Exception e){System.out.println(e);}
-			try {TimeUnit.SECONDS.sleep(1);}
-			catch(InterruptedException e){System.out.println(e);}
-		}
-	}
+        str = (String)din.readLine();
+        System.out.println("RCVD: "+str);
+
+        dout.close();
+        s.close();
+      }
+      catch(Exception e){System.out.println(e);}
+      try {TimeUnit.SECONDS.sleep(1);}
+      catch(InterruptedException e){System.out.println(e);}
+    }
+  }
 }
