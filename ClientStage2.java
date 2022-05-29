f6401dbf8a5ae9e1078ccf7826de64fb98b24404
Nexus237 <rohangeorge201@gmail.com>
2022-05-29 22:33:29 +1000

    Add Test Suite/Files (Stage #1 and #2)

--- ClientStage2.java
+++ ClientStage2.java
@@ -52,32 +52,31 @@ public class ClientStage2 {
                 str = (String) din.readLine();
                 System.out.println("RCVD: '" + str + "'");
 
-                String[] currCommand = str.split(" "); // Split the received command into a string array (core, memory,
-                                                       // disk, etc.)
+                String[] currCommand = str.split(" "); // Split the received command into a string array (core, memory, disk, etc.)
                 String currCommandType = currCommand[0]; // Command Type (either 'JOBN' or 'NONE')
 
                 while (true) {
                     // For JOBN, store the information of the job to variables
+                    // JOBN submitTime jobID estRuntime core memory disk
                     String jobID = currCommand[2];
                     String core = currCommand[currCommand.length - 3];
                     String memory = currCommand[currCommand.length - 2];
                     String disk = currCommand[currCommand.length - 1];
-                    System.out.println(ANSI_GREEN + "\n# Core: " + core + ", Memory: " + memory + ", Disk: " + disk
-                            + "\n" + ANSI_RESET);
+                    System.out.println(ANSI_GREEN + "\n# Core: " + core + ", Memory: " + memory + ", Disk: " + disk + "\n" + ANSI_RESET);
 
-                    // Queries the server information based on core, memory & disk
+                    // Queries the information based on core, memory & disk to find an available server
                     dout.write(("GETS Avail " + core + " " + memory + " " + disk + "\n").getBytes());
                     dout.flush();
                     System.out.println("SENT: 'GETS Avail " + core + " " + memory + " " + disk + "'");
 
-                    str = (String) din.readLine();
+                    str = (String) din.readLine(); 
                     System.out.println("RCVD: '" + str + "'");
 
                     String[] DATA = str.split(" "); // DATA nRecs recLen
-                    int records = Integer.parseInt(DATA[1]); // Number of records derived from the response ('DATA') of
-                                                             // the server
+                    int records = Integer.parseInt(DATA[1]); // Number of servers derived from the response ('DATA')
                     System.out.println(ANSI_GREEN + "\n# Number of Records: " + records + "\n" + ANSI_RESET);
 
+                    // If no servers are available for the requested job, find a server that is capable to run the job
                     if (records == 0) {
                         dout.write(("OK\n").getBytes());
                         dout.flush();
@@ -85,11 +84,11 @@ public class ClientStage2 {
 
                         str = (String) din.readLine();
                         System.out.println("RCVD: '" + str + "'");
-
+                        
+                        // Queries the information based on core, memory & disk to find a capable server
                         dout.write(("GETS Capable " + core + " " + memory + " " + disk + "\n").getBytes());
                         dout.flush();
-                        System.out.println(ANSI_RED + "SENT: 'GETS Capable " + core + " " + memory + " " + disk + "'"
-                                + ANSI_RESET);
+                        System.out.println(ANSI_RED + "SENT: 'GETS Capable " + core + " " + memory + " " + disk + "'" + ANSI_RESET);
 
                         str = (String) din.readLine();
                         System.out.println("RCVD: '" + str + "'");
@@ -113,14 +112,14 @@ public class ClientStage2 {
 
                         String[] currServer = str.split(" ");
 
+                        // Find the first server type and server ID that can run the job
                         if (i == 0) {
                             firstServerType = currServer[0];
                             firstServerID = Integer.parseInt(currServer[1]);
                         }
                     }
 
-                    System.out.println(ANSI_GREEN + "\n# First Server Type: " + firstServerType + ", First Server ID: "
-                            + firstServerID + "\n" + ANSI_RESET);
+                    System.out.println(ANSI_GREEN + "\n# First Server Type: " + firstServerType + ", First Server ID: " + firstServerID + "\n" + ANSI_RESET);
 
                     dout.write(("OK\n").getBytes());
                     dout.flush();
@@ -128,7 +127,8 @@ public class ClientStage2 {
 
                     str = (String) din.readLine();
                     System.out.println("RCVD: '" + str + "'");
-
+                    
+                    // Scheduling a job based on the job ID and the first server information (type and ID)
                     dout.write(("SCHD " + jobID + " " + firstServerType + " " + firstServerID + "\n").getBytes());
                     dout.flush();
                     System.out.println("SENT: 'SCHD " + jobID + " " + firstServerType + " " + firstServerID + "'");
@@ -143,7 +143,7 @@ public class ClientStage2 {
                     str = (String) din.readLine();
                     System.out.println("RCVD: '" + str + "'");
 
-                    currCommand = str.split(" "); // Split the received command (Checking for 'JOBN' or 'NONE')
+                    currCommand = str.split(" "); // Split the received command (Checking for 'JCPL' or 'JOBN' or 'NONE')
                     currCommandType = currCommand[0];
 
                     while (currCommandType.equals("JCPL")) {
@@ -154,14 +154,14 @@ public class ClientStage2 {
                         str = (String) din.readLine();
                         System.out.println("RCVD: '" + str + "'");
 
-                        currCommand = str.split(" "); // Split the received command (Checking for 'JOBN' or 'NONE')
+                        currCommand = str.split(" "); // Split the received command (Checking for 'JCPL' or 'JOBN' or 'NONE')
                         currCommandType = currCommand[0];
                     }
 
-                    if (currCommandType.equals("NONE")) {
+                    if (currCommandType.equals("NONE")) { // If no more jobs, break the loop
                         break;
                     } else {
-                        jobID = currCommand[2];
+                        jobID = currCommand[2]; //If there are more jobs, store the job ID to variable for the next while iteration
                     }
 
                 }
